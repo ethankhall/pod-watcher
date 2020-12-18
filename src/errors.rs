61f74c6da707492977ff5f673b4d2bc431a53409
Ethan Hall <ethankhall@pm.me>
2020-12-17 20:28:11 -0800

    Support shutting down Istio vs deleting the pod

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,11 @@
+use error_chain::error_chain;
+
+error_chain! {
+    links {
+        Kubernetes(kubernetes::Error, kubernetes::ErrorKind);
+        HTTP(reqwest::Error, reqwest::ErrorKind);
+    }
+}
 pub mod kubernetes {
     use error_chain::error_chain;
 
@@ -16,6 +24,25 @@ pub mod kubernetes {
                 description("unable to communicate with k8s")
                 display("unable to communicate with k8s because: '{}'", t)
             }
+        }
+    }
+}
+
+
+pub mod reqwest {
+    use error_chain::error_chain;
+
+    error_chain! {
+        types {
+        }
+
+        foreign_links {
+            Fmt(::std::fmt::Error);
+            Io(::std::io::Error) #[cfg(unix)];
+            Http(::reqwest::Error);
+        }
+
+        errors {
         }
     }
 }
